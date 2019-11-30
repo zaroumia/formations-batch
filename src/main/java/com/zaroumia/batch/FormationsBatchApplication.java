3ad2869a672776cda1a7ec110eff583febc90803
Momo <simo1705@gmail.com>
2019-11-30 18:22:59 +0100

    Gestion des paramétres : Utiliser un validateur de Spring Batch

--- src/main/java/com/zaroumia/batch/FormationsBatchApplication.java
+++ src/main/java/com/zaroumia/batch/FormationsBatchApplication.java
@@ -1,11 +1,9 @@
 package com.zaroumia.batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class FormationsBatchApplication {
 
 	public static void main(final String[] args) {
