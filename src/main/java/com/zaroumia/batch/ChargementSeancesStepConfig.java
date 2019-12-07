e17f14b4968a818e840e785bc3468af513a82ec1
Momo <simo1705@gmail.com>
2019-12-07 21:12:43 +0100

    Chargement des séances : Ignorer les lignes en erreur

--- src/main/java/com/zaroumia/batch/ChargementSeancesStepConfig.java
+++ src/main/java/com/zaroumia/batch/ChargementSeancesStepConfig.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -96,11 +97,18 @@ public class ChargementSeancesStepConfig {
 	}
 
 	@Bean
+	public SkipPolicy seanceSkipPolicy() {
+		return new SeanceSkipPolicy();
+	}
+
+	@Bean
 	public Step chargementSeancesCsvStep(final StepBuilderFactory stepBuilderFactory) {
 		return stepBuilderFactory.get("chargementSeancesCsvStep")
 				.<Seance, Seance>chunk(10)
 				.reader(seanceCsvItemReader(null))
 				.writer(seanceItemWriter(null))
+				.faultTolerant()
+				.skipPolicy(seanceSkipPolicy())
 				.listener(chargementSeancesStepListener())
 				.build();
 	}
@@ -111,6 +119,8 @@ public class ChargementSeancesStepConfig {
 				.<Seance, Seance>chunk(10)
 				.reader(seanceTxtItemReader(null))
 				.writer(seanceItemWriter(null))
+				.faultTolerant()
+				.skipPolicy(seanceSkipPolicy())
 				.listener(chargementSeancesStepListener())
 				.build();
 	}
---
+++ src/main/java/com/zaroumia/batch/SeanceSkipPolicy.java
@@ -0,0 +1,18 @@
+package com.zaroumia.batch;
+
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
+import org.springframework.batch.core.step.skip.SkipPolicy;
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class SeanceSkipPolicy implements SkipPolicy {
+
+	@Override
+	public boolean shouldSkip(final Throwable t, final int skipCount) throws SkipLimitExceededException {
+
+		if (t instanceof DataIntegrityViolationException && skipCount < 10) {
+			return true;
+		}
+		return false;
+	}
+
+}
